f6ba86b975c40d38c14a3cbca0518e77acd8058d
Crowdbotics <team@crowdbotics.com>
2022-02-11 22:16:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "homework_remind_33552",
+  url: "https://homework_remind_33552.botics.co",
+  api: "https://homework_remind_33552.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
